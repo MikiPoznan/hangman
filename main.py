e4676821a6e2967f2e8753a003bd2eaa80823dfc
MikiPoznan <55251314+MikiPoznan@users.noreply.github.com>
2023-03-21 19:29:50 +0100

    Przykładowa lista słów i tematów oraz kod wisielca

---
+++ main.py
@@ -0,0 +1,79 @@
+import json
+import random
+
+
+f = open("words.json")
+data = json.load(f)
+klucze = list(data.keys())
+######## Losowość \/ #########
+def losowanie():
+    los_temat = random.randint(0,len(klucze)-1)
+    a = data[klucze[los_temat]]
+    los_haslo = random.randint(0,len(a)-1)
+    return  data[klucze[los_temat]][los_haslo],klucze[los_temat]
+
+class gra():
+    def __init__(self,hp=10):
+        self.hp = hp
+        self.word = "x"
+        self.secret  = "-"
+        self.word_li = ['x']
+        self.guessed = []
+    def set_word(self, word):
+        secret = ""
+        word_li = list(word.lower())
+        for j in word_li:
+            if (j == " "):
+                secret += " "
+            else:
+                secret += "_"
+        self.secret = secret
+        self.word_li = word_li
+        self.word = word
+    
+    def guess_letter(self,letter):
+        if letter == self.word:
+            self.secret = letter
+            return
+        guessed = self.guessed
+        secret = ""
+        word_li = self.word_li
+        if not letter in guessed: 
+            self.guessed.append(letter)
+            for j in word_li:
+                if j in guessed:
+                    secret += j
+                elif (j == " "):
+                    secret += " "
+                else:
+                    secret += "_"
+
+            self.secret = secret
+        if not letter in self.word_li:
+            self.hp -= 1
+        
+            
+
+
+
+
+def zagraj():
+    haslo, temat = losowanie()
+    a = gra(10)
+    a.set_word(haslo)
+    while(True):   
+        print(f"\n"*50)
+        print(f"Kategoria: {temat}")
+        print(f"HP: {a.hp} \n{a.secret}")
+        text = input(f"Podaj Litere \n> ").strip()
+        a.guess_letter(text)
+        if a.hp == 0:
+            print("ale bambik. Nie masz życiek")
+            break 
+        if a.secret ==  a.word.lower():
+            print(f'Zgadłeś hasło: "{a.secret}"!!! Gratulacje ;)')
+            break
+        
+while(True):
+    zagraj()
+    
